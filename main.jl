4d1ff0b62a6b4dfe26c7caba82133db4eb06feca
Jesús Mejía <jesusmejded@gmail.com>
2018-05-22 19:08:55 -0500

    first attemp

---
+++ main.jl
@@ -0,0 +1,67 @@
+using Metaheuristics
+using BilevelBenchmark
+
+function genBounds(uBounds, lBounds, d)
+
+    return [repmat(uBounds[:,1], 1, d) repmat(uBounds[:,2], 1, d)]',
+           [repmat(lBounds[:,1], 1, d) repmat(lBounds[:,2], 1, d)]'
+end
+
+# configures problem
+function getBilevel(fnum::Int = 1)
+    D = 2
+    d = div(D, 2)
+
+    lower_D = upper_D = D
+
+    if fnum == 1
+        ub = [-5 10; -5 10.0]
+        lb = [-5 10; -π/2 π/2]
+    elseif fnum == 2
+        ub = [ -5.0    -5; 10.0  1]
+        lb = [ -5.0  0.01; 10.0  e]
+    elseif fnum == 4
+        ub = [-5.0 10; -1  1]
+        lb = [-5.0 10;  0  e]
+    elseif fnum == 5
+        ub = [-5.0 10; -5.0  10.0]
+        lb = [-5.0 10; -5.0  10.0]
+    end
+    
+    upper_bounds, lower_bounds = genBounds( ub, lb, d ) 
+
+    # leader
+    F(x::Array{Float64}, y::Array{Float64}) = bilevel_leader(x, y, fnum)
+
+    # follower
+    f(x::Array{Float64}, y::Array{Float64}) = bilevel_follower(x, y, fnum)
+    
+    return f, F, lower_D, upper_D, lower_bounds, upper_bounds
+end
+
+function main()
+    # x: upper level
+    # y: lower level
+    fnum = 1
+
+    # problem settings
+    f, F, lower_D, upper_D, lower_bounds, upper_bounds = getBilevel(fnum)
+
+    println(lower_bounds)
+    println(upper_bounds)
+
+    # objective function
+    Fobj(x) = begin
+        # y ∈ arg min { f(x, z) : z ∈ Y }
+        y, fval = eca( z-> f(x, z), lower_D; showResults=false, limits=lower_bounds)
+
+        # return upper level value
+        return F(x, y)
+    end
+
+    # optimize
+    x, f = eca(Fobj, upper_D; limits=upper_bounds, max_evals=1000, showIter=true)
+
+end
+
+main()
