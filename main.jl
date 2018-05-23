f2d93d5b41e174a6c9cd3903d6a9ec923b7eb9a2
Jesus Mejia <jesusmejded@gmail.com>
2018-05-22 20:17:10 -0500

    fix genBounds

--- main.jl
+++ main.jl
@@ -3,8 +3,8 @@ using BilevelBenchmark
 
 function genBounds(uBounds, lBounds, d)
 
-    return [repmat(uBounds[:,1], 1, d) repmat(uBounds[:,2], 1, d)]',
-           [repmat(lBounds[:,1], 1, d) repmat(lBounds[:,2], 1, d)]'
+    return [repmat(uBounds[:,1], 1, d) repmat(uBounds[:,2], 1, d)],
+           [repmat(lBounds[:,1], 1, d) repmat(lBounds[:,2], 1, d)]
 end
 
 # configures problem
@@ -28,7 +28,7 @@ function getBilevel(fnum::Int = 1)
         lb = [-5.0 10; -5.0  10.0]
     end
     
-    upper_bounds, lower_bounds = genBounds( ub, lb, d ) 
+    upper_bounds, lower_bounds = genBounds( ub', lb', d ) 
 
     # leader
     F(x::Array{Float64}, y::Array{Float64}) = bilevel_leader(x, y, fnum)
@@ -42,25 +42,22 @@ end
 function main()
     # x: upper level
     # y: lower level
-    fnum = 1
+    fnum = 5
 
     # problem settings
     f, F, lower_D, upper_D, lower_bounds, upper_bounds = getBilevel(fnum)
 
-    println(lower_bounds)
-    println(upper_bounds)
-
     # objective function
     Fobj(x) = begin
         # y ∈ arg min { f(x, z) : z ∈ Y }
-        y, fval = eca( z-> f(x, z), lower_D; showResults=false, limits=lower_bounds)
+        y, fval = eca( z-> f(x, z), lower_D; showResults=false, limits=lower_bounds,max_evals=1000lower_D)
 
         # return upper level value
         return F(x, y)
     end
 
     # optimize
-    x, f = eca(Fobj, upper_D; limits=upper_bounds, max_evals=1000, showIter=true)
+    x, f = eca(Fobj, upper_D; limits=upper_bounds, max_evals=500upper_D, showIter=true)
 
 end
 
