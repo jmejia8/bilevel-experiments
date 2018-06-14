15da2ad147f3691d319cdd5c04d92c2355c896ad
Jesús Mejía <jesusmejded@gmail.com>
2018-06-14 01:04:32 -0500

    save convergence

--- BCA.jl
+++ BCA.jl
@@ -1,22 +1,36 @@
-using Metaheuristics
+import Metaheuristics: eca
 
 function BCA(F::Function, f::Function, upper_D, lower_D, upper_bounds, lower_bounds)
+    K = 7
+    N = 2*K*lower_D
+
     Fobj(x) = begin
         # y ∈ arg min { f(x, z) : z ∈ Y }
-        y, fval = eca( z-> f(x, z), lower_D; showResults=false,
+        y, fval = eca( z-> f(x, z), lower_D; K = K, N = N,
+                                             showResults=false,
                                              adaptive=true,
                                              canResizePop=true,
                                              limits=lower_bounds,
-                                             max_evals=1000lower_D)
-
+                                             max_evals=250lower_D)
         # return upper level value
-        return F(x, y)
+        return F(x, y)# + 10fval
     end
 
     # optimize
-    x, F = eca(Fobj, upper_D;
+    x, Fv = eca(Fobj, upper_D;
                     limits=upper_bounds,
+                    showResults=false,
                     adaptive=true,
-                    max_evals=200upper_D,
-                    showIter=true)
+                    max_evals=250upper_D,
+                    saveConvergence="output/leader$(current_f)_r$(current_r).csv",
+                    saveLast="output/leader$(current_f)_last_r$(current_r).csv",
+                    showIter=false)
+
+    y, fv = eca( z-> f(x, z), lower_D; showResults=false,
+                                             adaptive=true,
+                                             canResizePop=false,
+                                             limits=lower_bounds,
+                                             max_evals=250lower_D)
+
+    return x, F(x,y), f(x,y)
 end
